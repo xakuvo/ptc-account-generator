017ceb8d101f55d64659e7f0b21f7822c1ec6669
Anthony Anderson2 <aanderson2@bridge.local>
2016-08-07 19:57:21 -0600

    loop

--- index.js
+++ index.js
@@ -5,7 +5,10 @@ require('babel-register');
 var fs = require('fs');
 var createAccount = require('./src/ptc-account-generator').default
 var STORE_FILE = './accounts.txt';
+var count = 0;
 
-createAccount()
-  .then(res => fs.appendFileSync(STORE_FILE, "\n\n" + JSON.stringify(res)))
-  .catch(err => console.error(err));
+while (count<=200){
+  createAccount()
+    .then(res => fs.appendFileSync(STORE_FILE, "\n\n" + JSON.stringify(res)))
+    .catch(err => console.error(err));
+}
